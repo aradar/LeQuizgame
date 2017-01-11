a21bcba4cc178c542dc4f8d1e79de00572f86529
rschlett <ralphschlett@gmail.com>
2017-01-11 17:06:13 +0100

    added the server to the app repo

---
+++ server/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import com.google.gson.Gson;
+
+import static spark.Spark.*;
+
+/**
+ * Created by rschlett on 1/6/17.
+ */
+public class Main {
+
+    public static class HighScoreEntity {
+        public int position;
+        public String name;
+        public int movesTaken;
+    }
+
+    public static String getHighScoreEntities(int number) {
+        HighScoreEntity[] highScoreEntities = new HighScoreEntity[number];
+        for (int i = 0; i < highScoreEntities.length; i++) {
+            HighScoreEntity highScoreEntity = new HighScoreEntity();
+            highScoreEntity.position = i + 1;
+            highScoreEntity.movesTaken = (int) (Math.random() * 20);
+            highScoreEntity.name = "Hasso";
+            highScoreEntities[i] = highScoreEntity;
+        }
+
+        return new Gson().toJson(highScoreEntities);
+    }
+
+    public static void main(String[] args) {
+        get("/top/:number", (req, res) -> {
+            int number = Integer.valueOf(req.params(":number"));
+            res.type("application/json");
+            return getHighScoreEntities(number);
+        });
+
+        get("/all", (req, res) -> {
+            res.type("application/json");
+            return getHighScoreEntities(5);
+        });
+        get("/name/:name", (req, res) -> {
+            res.type("application/json");
+
+            String name = req.params(":name");
+            HighScoreEntity highScoreEntity = new HighScoreEntity();
+            highScoreEntity.position = (int) (Math.random() * 10);
+            highScoreEntity.movesTaken = (int) (Math.random() * 20);
+            highScoreEntity.name = name;
+
+            return new Gson().toJson(highScoreEntity);
+        });
+        put("/save", (req, res) -> {
+            System.out.println(req.body());
+           return "GEHT SCHON";
+        });
+    }
+}
